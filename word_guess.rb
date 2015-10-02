bb3393f2598988f1eb4590a9d77e46121d6e35d1
Logan McDonald <loganmcdona11@gmail.com>
2015-10-02 14:07:33 -0700

    Fixed some of the bugs in word_guess.rb

--- word_guess.rb
+++ word_guess.rb
@@ -1,24 +1,16 @@
 require "colorize"
-require "./word_guess_art.rb"
-
-MAX_GUESSES = 6
 
 class WordGuess
 	def initialize
 		@words = ["test"]
 		@hidden_word = @words[rand(@words.length)]
-		@wrong_answers = []
-		@count_wrong = 0 
+		@hidden_word_array = @hidden_word.split('')
 		@right_answers = []
+		@wrong_answers = []
+		@count_wrong = 0
 		@blank_row = "_ " * @hidden_word.length
 		@answer_output = @blank_row.split(' ')
-		@hidden_word_array = @hidden_word.split('')
 	
-		puts @blank_row
-		puts user_input
-	end
-
-	def display
 		introduction = <<YES
 			     <----->
    			    <  (0)  >
@@ -33,49 +25,50 @@ class WordGuess
 
 	The robots are coming to 'TERMINATE' mankind!
 	Please help us guess the word before we all perish.
+	We have given you blank spaces to make things easier.
+	We hear humans like to do it the easy way.\n
 
 YES
 
 		puts introduction 
-		puts @blank_row
+		puts @blank_row.center(65)
 		puts user_input
 	end
 
 	def user_input
 		until @hidden_word_array == @answer_output
-			print ascii_art
+			puts ascii_art
 
 			puts "\n Enter a letter to guess. Try not to guess wrong."
 			puts "Right guesses: #{@right_answers}."
 			puts "Wrong guesses: #{@wrong_answers}"
 
 			@letter = gets.chomp.downcase
+
 			puts check_user_input
 
 			# while @wrong_answers.length < MAX_GUESSES
-				if @hidden_word_array.include?(@letter)
-					# times_in_array = @hidden_word_array.count(letter)
-					# add_letters = [letter] * times_in_array
-					# @right_answers += add_letters
+			if @hidden_word_array.include?(@letter)
+				# times_in_array = @hidden_word_array.count(letter)
+				# add_letters = [letter] * times_in_array
+				# @right_answers += add_letters
 
-					@hidden_word_array.each_index do |x|
-						if @right_answers[x] == @letter
-							@answer_output[x] = @letter
-						end
+				@hidden_word_array.each_index do |x|
+					if @hidden_word_array[x] == @letter
+						@answer_output[x] = @letter
 					end
-
-				else
-					puts "Nope! That's not right!"
-					@wrong_answers << @letter
-					@count_wrong += 1
 				end
 
-				puts "Right: #{@right_answers}"
-				puts "Wrong: #{@wrong_answers}"
-				
-				@letter = gets.chomp.downcase
-			# end
+			else
+				puts "Nope! That's not right!"
+				@count_wrong += 1
+				@wrong_answers << @letter
+			end
+
+			puts @answer_output.join(' ')
 		end
+
+		puts "You have won"
 	end
 
 	def check_user_input
